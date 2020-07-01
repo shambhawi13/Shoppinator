59217cf06ab0b97df9f0ce806d6f4138ed932587
Sham <shambhawi.kumari1393@gmail.com>
2020-06-30 22:22:34 -0700

    added test accordian to be referenced in another branch

---
+++ script.js
@@ -0,0 +1,22 @@
+// var settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://google-ai-vision.p.rapidapi.com/cloudVision/imageObjectDetection",
+// 	"method": "POST",
+// 	"headers": {
+// 		"x-rapidapi-host": "google-ai-vision.p.rapidapi.com",
+// 		"x-rapidapi-key": "c6064b5170msh977e24b1ae6c804p17fd8djsnf539fedd1d13",
+// 		"content-type": "application/json",
+// 		"accept": "application/json"
+// 	},
+// 	"processData": false,
+// 	"data": "{ \"source\": \"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRyTqyx98Kh8VjAwJ7Ud-k1XuKiibR_SuprGbsBln-tl-uhVaNkw7FIzEPWCA&usqp=CAc\", \"sourceType\": \"url\"}"
+// }
+
+// $.ajax(settings).done(function (response) {
+// 	console.log(response);
+// });
+
+$('.ui.accordion')
+  .accordion()
+;

808f755fbb850573b4b3b5ad64599fc4bc476138
Sham <shambhawi.kumari1393@gmail.com>
2020-07-01 16:33:55 -0700

    added functionality to select file from file explorer

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+/* Search Page Logic here */
+
 $('.ui.accordion')
   .accordion()
 ;
+
+function uploadImage(){
+    document.getElementById('fileInput').click();
+}
+
+
+/* Search Page Logic ends here */

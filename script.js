8273be0f9f0a3937d9854c9e52df9da05aea9c3d
Sham <shambhawi.kumari1393@gmail.com>
2020-07-02 00:56:36 -0700

    added func to convert image to b64

--- script.js
+++ script.js
@@ -1,13 +1,101 @@
 /* Search Page Logic here */
 var rapidAPIKey = 'c6064b5170msh977e24b1ae6c804p17fd8djsnf539fedd1d13';
+//default value on page load
+var storeType = 'online';
+var accordSelected = 'image';
+var b64;
 
 $('.ui.accordion')
     .accordion()
     ;
 
+$('.toggle-store-online input[type="checkbox"]').click(function () {
+    if ($(this).is(":checked")) {
+        storeType = 'store';
+        console.log("Checkbox is checked." + storeType);
+    }
+    else if ($(this).is(":not(:checked)")) {
+        storeType = 'online';
+        console.log("Checkbox is unchecked." + storeType);
+    }
+});
+
+$('#imageAccord').on('click', () => {
+    accordSelected = 'image';
+    console.log("Image accord selected: " + accordSelected);
+});
+
+$('#textAccord').on('click', () => {
+    accordSelected = 'text';
+    console.log("Text accord selected: " + accordSelected);
+});
+
 function uploadImage() {
     document.getElementById('fileInput').click();
 }
+
+//converts image to base 64
+function readFile() {
+    if (this.files && this.files[0]) {
+        var FR = new FileReader();
+        $(FR).on("load", function (e) {
+            //console.log('b64 result: ' + e.target.result);
+            b64 = e.target.result;
+
+        });
+        FR.readAsDataURL(this.files[0]);
+    }
+}
+
+$("#fileInput").on("change", readFile);
+
+$('.submit-button').on('click', (event) => {
+    event.preventDefault();
+    var resultObject;
+
+    if (accordSelected === 'image') {
+        var imageUrl = $('.form-image-url input').val().trim();
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://google-ai-vision.p.rapidapi.com/cloudVision/imageObjectDetection",
+            "method": "POST",
+            "headers": {
+                "x-rapidapi-host": "google-ai-vision.p.rapidapi.com",
+                "x-rapidapi-key": rapidAPIKey,
+                "content-type": "application/json",
+                "accept": "application/json"
+            },
+            "processData": false,
+            "data": "{ \"source\":\"" + imageUrl + "\", \"sourceType\": \"url\"}"
+        }
+
+        console.log(settings);
+        var storesResponse = JSON.parse(localStorage.getItem('scanned-image'));
+        //to be removed later
+        if (storesResponse) {
+            console.log('Stored in local storage');
+            //lodash to map names from response
+            console.log(_.map(storesResponse, 'name'));
+            resultObject = _.map(storesResponse, 'name');
+        }
+        else {
+            $.ajax(settings).done(function (response) {
+                console.log(response);
+                localStorage.setItem('scanned-image', JSON.stringify(response.objects));
+                resultObject = _.map(response.objects, 'name');
+            });
+        }
+
+    }
+    else if (accordSelected === 'text') {
+        resultObject = [];
+        let productName = $('#product-name').val().trim();
+        resultObject.push(productName);
+    }
+
+});
+
 
 // var settings = {
 //     "async": true,
@@ -23,44 +111,5 @@ function uploadImage() {
 //     "processData": false,
 //     "data": "{ \"source\": \"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRyTqyx98Kh8VjAwJ7Ud-k1XuKiibR_SuprGbsBln-tl-uhVaNkw7FIzEPWCA&usqp=CAc\", \"sourceType\": \"url\"}"
 // }
-
-$('.submit-button').on('click', (event) => {
-    event.preventDefault();
-    var imageUrl = $('.form-image-url input').val().trim();
-    console.log('Submit clicked', imageUrl);
-
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://google-ai-vision.p.rapidapi.com/cloudVision/imageObjectDetection",
-        "method": "POST",
-        "headers": {
-            "x-rapidapi-host": "google-ai-vision.p.rapidapi.com",
-            "x-rapidapi-key": rapidAPIKey,
-            "content-type": "application/json",
-            "accept": "application/json"
-        },
-        "processData": false,
-        "data": "{ \"source\":\"" + imageUrl + "\", \"sourceType\": \"url\"}"
-    }
-
-    console.log(settings);
-    
-    console.log(_.chunk(['a', 'b', 'c', 'd'], 2));
-    var storesResponse = JSON.parse(localStorage.getItem('scanned-image'));
-    //to be removed later
-    if(storesResponse){
-        console.log('Stored in local storage');
-        //lodash to map names from response
-        console.log(_.map(storesResponse, 'name'));
-    }
-    else{
-        $.ajax(settings).done(function (response) {
-            console.log(response);
-            localStorage.setItem('scanned-image',JSON.stringify(response.objects));
-        });
-    }
-})
-
 
 /* Search Page Logic ends here */
